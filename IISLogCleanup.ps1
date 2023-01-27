a2197face2ac79f3cc0829c58c5ff4ce4abed7b6
Mikkel Thodsen <123701392+ITR-MITHO@users.noreply.github.com>
2023-01-27 08:29:49 +0000

    Create IISLogCleanup.ps1

---
+++ IISLogCleanup.ps1
@@ -0,0 +1,25 @@
+<#
+
+Will delete Exchange IIS-logs older then 14-days
+
+#>
+
+Import-Module WebAdministration
+$Date = (Get-Date).AddDays(-14)
+$Folder = Get-ItemProperty "IIS:\Sites\Default Web Site" -name logFile.directory | Select Value
+If ($Folder.Value -like "%Systemdrive%*")
+
+{
+
+    Get-ChildItem C:\Inetpub\Logs\LogFiles -Recurse | Where-Object {$_.LastWriteTime -lt $Date} | Remove-Item -Force
+    
+
+}
+
+ Else
+
+{
+    
+    Get-ChildItem $Folder.Value -Recurse | Where-Object {$_.LastWriteTime -lt $Date} | Remove-Item -Force
+
+}

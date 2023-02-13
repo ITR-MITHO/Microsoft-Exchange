0684f4aa8a9ab1a0f1734b90c7c28423d9fbee5f
Mikkel Thodsen <123701392+ITR-MITHO@users.noreply.github.com>
2023-02-13 11:36:48 +0100

    Update EXOCalenderPermissions.ps1

--- EXOCalenderPermissions.ps1
+++ EXOCalenderPermissions.ps1
@@ -1,22 +1,14 @@
 <# 
  
 .DESCRIPTION  
-This script gives X (user or group) access to calendars for X users.
+This script gives all users reviewer access to eachother.
 Default is all users, the accessrights can be changed under variables, so can the user/group.
  
- 
-.OUTPUTS 
-It has a log, other than that, it just changes calendar permissions.
-It's got a test function, if you remove the #/Hashtag from line 114 (the one that changes permissions)
- 
- it outputs the file to "C:\office365Scripts\Keys\Credentials.txt"
-If the file is not present, one will be made.
 #>
- 
- 
-#Variables
+
+
 # Import the Exchange Online Management module
-$UserName = "admin@onmicrosoft.com"
+$Username = "admin@onmicrosoft.com"
 Import-Module ExchangeOnlineManagement
 
 # Store the credentials for the connection in a secure file
@@ -38,12 +30,14 @@ else {
     Write-Host "Failed to connect to Exchange Online."
 }
 
-############################################# START OF ACTUAL SCRIPT #############################################
 
- 
- 
-foreach($mbx in Get-Mailbox -ResultSize Unlimited -RecipientTypeDetails UserMailbox){
+# Reviewer Permissions for everyone to UserMailboxes
+$UserToGiveAccess = 'Default'
+$AccessRight = 'Reviewer'
+foreach ($mbx in Get-Mailbox -ResultSize Unlimited -RecipientTypeDetails UserMailbox)
+{
+
     $languageCalendar = (Get-MailboxFolderStatistics -Identity $mbx.userprincipalname -FolderScope Calendar | Select-Object -first 1).name
     Set-MailboxFolderPermission -Identity ($mbx.UserPrincipalName+":\$LanguageCalendar") -User $UserToGiveAccess -AccessRights $AccessRight
-    Get-MailboxFolderPermission -Identity ($mbx.UserPrincipalName+":\$LanguageCalendar") -User $UserToGiveAccess | Select-Object identity,accessrights
+
 }

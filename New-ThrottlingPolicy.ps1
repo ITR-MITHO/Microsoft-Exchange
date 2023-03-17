6ca09c908b0d3ba612473c1bef28e8e9361b02f9
Mikkel Thodsen <123701392+ITR-MITHO@users.noreply.github.com>
2023-03-17 08:02:46 +0000

    Update New-ThrottlingPolicy.ps1

--- New-ThrottlingPolicy.ps1
+++ New-ThrottlingPolicy.ps1
@@ -5,8 +5,8 @@ The policy deletes ActiveSync Devices that haven't synced in over 30 days.
 
 #>
 
-New-ThrottlingPolicy -Name "Default Policy"
-Set-ThrottlingPolicy "Default Policy" -EasMaxInactivityForDeviceCleanup 30
+New-ThrottlingPolicy -Name "Default ThrottlingPolicy"
+Set-ThrottlingPolicy "Default ThrottlingPolicy" -EasMaxInactivityForDeviceCleanup 30
 
 Foreach ($Mailbox in Get-Mailbox -Resultsize Unlimited -RecipientTypeDetails UserMailbox)
 {

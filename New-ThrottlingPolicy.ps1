1f462952a651c0cc2ffcb2c9c2cfc6d7875fdb01
Mikkel Thodsen <123701392+ITR-MITHO@users.noreply.github.com>
2023-03-17 08:01:48 +0000

    Update New-ThrottlingPolicy.ps1

--- New-ThrottlingPolicy.ps1
+++ New-ThrottlingPolicy.ps1
@@ -1,3 +1,10 @@
+<#
+
+Creates a new ThrottlingPolicy and assigns it to all mailboxes.
+The policy deletes ActiveSync Devices that haven't synced in over 30 days.
+
+#>
+
 New-ThrottlingPolicy -Name "Default Policy"
 Set-ThrottlingPolicy "Default Policy" -EasMaxInactivityForDeviceCleanup 30
 

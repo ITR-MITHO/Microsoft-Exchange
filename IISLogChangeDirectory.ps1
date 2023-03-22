59d6ad9146d78d13317dec20aff8d4b1aa5888b8
Mikkel Thodsen <123701392+ITR-MITHO@users.noreply.github.com>
2023-03-22 08:41:32 +0000

    Update IISLogChangeDirectory.ps1

--- IISLogChangeDirectory.ps1
+++ IISLogChangeDirectory.ps1
@@ -1,8 +1,10 @@
+$Location = Read-Host "Enter the new location. (e.g. F:\Logs\)"
+
 # Crate new IIS-directories
-New-Item -ItemType Directory -Path F:\inetpub\logs\LogFiles\W3SVC1
-New-Item -ItemType Directory -Path F:\inetpub\logs\LogFiles\W3SVC2
+New-Item -ItemType Directory -Path $Location
+New-Item -ItemType Directory -Path $Location
 
 # Sets IIS to log in the new directories  
 Import-Module WebAdministration
-Set-ItemProperty "IIS:\Sites\Default Web Site" -name logFile.directory  -value "F:\Inetpub\logs\LogFiles"
-Set-ItemProperty "IIS:\Sites\Exchange Back End" -name logFile.directory  -value "F:\Inetpub\logs\LogFiles"
+Set-ItemProperty "IIS:\Sites\Default Web Site" -name logFile.directory  -value "$Location"
+Set-ItemProperty "IIS:\Sites\Exchange Back End" -name logFile.directory  -value "$Location"

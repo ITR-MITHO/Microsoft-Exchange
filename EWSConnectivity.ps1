9bb6ae2a2864a9f0d4f04520d93ad2d2fcf3d35f
Mikkel Thodsen <123701392+ITR-MITHO@users.noreply.github.com>
2023-05-17 13:57:08 +0000

    Update EWSConnectivity.ps1

--- EWSConnectivity.ps1
+++ EWSConnectivity.ps1
@@ -8,12 +8,16 @@ Used to see what mailboxes are using EWS and which aren't using it.
 
 #>
 
+# Prerequisites
 $Date = (Get-Date).AddDays(-30)
 $CSV = Get-Mailbox -ResultSize unlimited | Select SamAccountName, DisplayName, PrimarySMTPAddress
 CD "C:\Program Files\Microsoft\Exchange Server\V15\Logging\HttpProxy\Ews"
-Echo "Name, Username, Email" Out-File $home\desktop\YES-activity.txt
-Echo "Name, Username, Email" Out-File $home\desktop\NO-activity.txt
 
+# Creating our own CSV-file with data
+Echo "Name, Username, Email" | Out-File $home\desktop\YES-activity.txt
+Echo "Name, Username, Email" | Out-File $home\desktop\NO-activity.txt
+
+# Beginning to go through all mailboxes.
 ForEach ($M in $csv)
 {
 $Name = $M.SamAccountName

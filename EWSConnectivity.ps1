f117224f0fdca8b5354fe0350baf4eb9b763116d
Mikkel Thodsen <123701392+ITR-MITHO@users.noreply.github.com>
2023-05-17 13:53:14 +0000

    Update EWSConnectivity.ps1

--- EWSConnectivity.ps1
+++ EWSConnectivity.ps1
@@ -9,23 +9,25 @@ Used to see what mailboxes are using EWS and which aren't using it.
 #>
 
 $Date = (Get-Date).AddDays(-30)
-$CSV = Get-Mailbox -ResultSize unlimited | Select SamAccountName, DisplayName
+$CSV = Get-Mailbox -ResultSize unlimited | Select SamAccountName, DisplayName, PrimarySMTPAddress
 CD "C:\Program Files\Microsoft\Exchange Server\V15\Logging\HttpProxy\Ews"
-Echo "Name, Username, Path" Out-File $home\desktop\YES-activity.txt
+Echo "Name, Username, Email" Out-File $home\desktop\YES-activity.txt
+Echo "Name, Username, Email" Out-File $home\desktop\NO-activity.txt
 
 ForEach ($M in $csv)
 {
 $Name = $M.SamAccountName
 $Full = $M.DisplayName
+$Primary = $M.PrimarySMTPAddress
 $UserActivity = Get-ChildItem -Recurse | Where {$_.LastWriteTime -gt $Date} | Select-String "$Name" | Select LineNumber, Path -last 1
 $Path = $UserActivity.Path
 If (-not $UserActivity)
 {
 
-Echo "$Full - $Name - have NO activity" | Out-File $home\desktop\NO-activity.txt -Append
+Echo "$Full, $Name, $Primary" | Out-File $home\desktop\NO-activity.txt -Append
 }
 Else
 {
-Echo "$Full, $Name, $Path" | Out-File $home\desktop\YES-activity.txt -Append
+Echo "$Full, $Name, $Primary" | Out-File $home\desktop\YES-activity.txt -Append
 }
  }

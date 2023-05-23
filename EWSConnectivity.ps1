bdb3b38451d8a5181651333f24ff7e11c3641e92
Mikkel Thodsen <123701392+ITR-MITHO@users.noreply.github.com>
2023-05-23 10:40:43 +0000

    Update EWSConnectivity.ps1

--- EWSConnectivity.ps1
+++ EWSConnectivity.ps1
@@ -9,10 +9,14 @@ Used to see what mailboxes are using EWS and which aren't using it.
 #>
 
 # Prerequisites
-$Date = (Get-Date).AddDays(-30)
-$CSV = Get-Mailbox -ResultSize unlimited | Select SamAccountName, DisplayName, PrimarySMTPAddress, LastLogonDate
+Add-PSSnapin *EXC*
+$Date = (Get-Date).AddDays(-14)
+$CSV = Get-Mailbox -ResultSize unlimited -RecipientTypeDetails UserMailbox, Sharedmailbox | Select SamAccountName, DisplayName, PrimarySMTPAddress, LastLogonDate
 CD "C:\Program Files\Microsoft\Exchange Server\V15\Logging\HttpProxy\Ews"
+$Data = Get-ChildItem -Recurse | Where {$_.LastWriteTime -gt $Date}
 
+cls
+Write-Host "Starting to check logs from $Date til now" -ForeGroundColor Yellow
 # Creating our own CSV-file with data
 Echo "Name, Username, Email, LastLogon, Activity" | Out-File $home\desktop\Activity.txt
 
@@ -25,15 +29,16 @@ $Name = $M.SamAccountName
 $Full = $M.DisplayName
 $Primary = $M.PrimarySMTPAddress
 $Logon = $AD.LastLogonDate
-$UserActivity = Get-ChildItem -Recurse | Where {$_.LastWriteTime -gt $Date} | Select-String "$Name" | Select LineNumber, Path -last 1
-$Path = $UserActivity.Path
+$UserActivity = $Data | Select-String "$Name"
 If (-not $UserActivity)
 {
 
-Echo "$Full, $Name, $Primary, $Logon, No" | Out-File $home\desktop\Activity.txt -Append
+Echo "$Full, $Name, $Primary, $Logon, No" | Out-File $home\desktop\Activity.csv -Append
 }
 Else
 {
-Echo "$Full, $Name, $Primary, $Logon, Yes" | Out-File $home\desktop\Activity.txt -Append
+Echo "$Full, $Name, $Primary, $Logon, Yes" | Out-File $home\desktop\Activity.csv -Append
 }
  }
+ cls
+ Write-Host "Completed. Find your file here: $home\desktop\Activity.txt" -ForeGroundColor Green

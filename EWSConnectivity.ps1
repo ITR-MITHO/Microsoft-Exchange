26f2bab88a6d709742c72629d3c3acb2494b889a
Mikkel Thodsen <123701392+ITR-MITHO@users.noreply.github.com>
2023-05-25 17:54:10 +0000

    Update EWSConnectivity.ps1

--- EWSConnectivity.ps1
+++ EWSConnectivity.ps1
@@ -1,38 +1,40 @@
-<# 
-
-.DESCRIPTION  
-Used to see what mailboxes are using EWS and which aren't using it.
-
-.OUTPUT
-1 file named "Activity.txt" will be placed on your desktop
-
-#>
-
-# Prerequisites
 Add-PSSnapin *EXC*
+Import-Module WebAdministration
+
+$Folder = Get-ItemProperty "IIS:\Sites\Default Web Site" -name logFile.directory | Select Value
 $Date = (Get-Date).AddDays(-14)
-$CSV = Get-Mailbox -ResultSize unlimited -RecipientTypeDetails UserMailbox, Sharedmailbox | Select SamAccountName, DisplayName, PrimarySMTPAddress, LastLogonDate
-CD "C:\Program Files\Microsoft\Exchange Server\V15\Logging\HttpProxy\Ews"
-$Data = Get-ChildItem -Recurse | Where {$_.LastWriteTime -gt $Date}
+$Mailbox = Get-Mailbox -ResultSize unlimited -RecipientTypeDetails UserMailbox, Sharedmailbox | Select SamAccountName, DisplayName, PrimarySMTPAddress, LastLogonDate
 
-cls
-Write-Host "Starting to check logs from $Date til now" -ForeGroundColor Yellow
 # Creating our own CSV-file with data
-Echo "Name, Username, Email, LastLogon, Activity" | Out-File $home\desktop\Activity.txt
-
+Echo "Name, Username, Email, LastLogon, Activity" | Out-File $home\desktop\Activity.csv
 
 # Beginning to go through all mailboxes.
-ForEach ($M in $csv)
+CLS
+Write-Host "Starting to collect logs.. This might take a while." -ForegroundColor Yellow
+ForEach ($M in $Mailbox)
 {
 $AD = Get-ADUser $M.SamAccountName -Properties LastLogonDate | Select LastLogonDate
 $Name = $M.SamAccountName
 $Full = $M.DisplayName
 $Primary = $M.PrimarySMTPAddress
 $Logon = $AD.LastLogonDate
-$UserActivity = $Data | Select-String "$Name"
-If (-not $UserActivity)
-{
 
+If ($Folder.Value -like "%Systemdrive%*")
+{
+    CD "C:\Inetpub\Logs\LogFiles"
+    # Change the line below with one of the following: */mapi/*, */Microsoft-Server-ActiveSync/*, */AutoDiscover/*, */OWA/*, */ECP/* and */EWS/*
+   $Data = Get-ChildItem -Recurse | Where {$_.LastWriteTime -GT $Date} | Select-String -Pattern "$Name" | Where {$_.Line -like "*/EWS/*"} | Select -First 1
+}
+ Else
+{  
+    CD $Folder.Value
+    # Change the line below with one of the following: */mapi/*, */Microsoft-Server-ActiveSync/*, */AutoDiscover/*, */OWA/*, */ECP/* and */EWS/*
+    $Data = Get-ChildItem -Recurse | Where {$_.LastWriteTime -GT $Date} | Select-String -Pattern "$Name" | Where {$_.Line -like "*/EWS/*"} | Select -First 1
+}
+
+
+If (-not $Data)
+{
 Echo "$Full, $Name, $Primary, $Logon, No" | Out-File $home\desktop\Activity.csv -Append
 }
 Else
@@ -40,5 +42,6 @@ Else
 Echo "$Full, $Name, $Primary, $Logon, Yes" | Out-File $home\desktop\Activity.csv -Append
 }
  }
+ 
  cls
  Write-Host "Completed. Find your file here: $home\desktop\Activity.txt" -ForeGroundColor Green

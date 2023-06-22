a299c6cab2770ae1bbc424422dadaa88247bae12
Mikkel Thodsen <123701392+ITR-MITHO@users.noreply.github.com>
2023-06-22 08:57:52 +0000

    Update DAGMaintenance.ps1

--- DAGMaintenance.ps1
+++ DAGMaintenance.ps1
@@ -10,17 +10,11 @@ Everything is done automatically, and works by simply inputting a number into th
 #>
 
 # Checking permissions
-$PMError = Test-Path $Home\desktop\PermissionIssue.txt
-if ($PMError)
-{
-Remove-Item "$Home\desktop\PermissionIssue.txt" -Force
-}
-timeout 3
+
 $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
 If (-not $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
 {
-echo "Start PowerShell as an Administrator" > $Home\desktop\PermissionIssue.txt
-Start $home\desktop\PermissionIssue.txt
+Write-Host "Start PowerShell as an Administrator" -ForeGroundColor Yellow
 Break
 }
 

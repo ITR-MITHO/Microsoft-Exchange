10e7653e1d4407c57616f75def9e2c8432f7d0e4
Mikkel Thodsen <123701392+ITR-MITHO@users.noreply.github.com>
2023-07-25 15:15:38 +0200

    Update SetupOverview.ps1

--- SetupOverview.ps1
+++ SetupOverview.ps1
@@ -119,14 +119,13 @@ Group Policies that might effect Outlook behaviour
 " -ForegroundColor Yellow
 
 $DC = (Get-ADDomainController | Select Name -First 1).Name
-Invoke-Command -ComputerName $DC 
-{
+Invoke-Command -ComputerName $DC {
 
 $AllGPO = Get-GPO -All -Domain $env:SERDNSDOMAIN
 [string[]] $MatchedGPOList = @()
 
 ForEach ($GPO in $AllGPO) { 
-    $Report = Get-GPOReport -Guid $gpo.Id -ReportType Xml 
+    $Report = Get-GPOReport -Guid $GPO.Id -ReportType XML 
     if ($Report -match 'Outlook') { 
         Write-Host "$($GPO.DisplayName)" -ForeGroundColor "Green"
         $MatchedGPOList += "$($GPO.DisplayName)";

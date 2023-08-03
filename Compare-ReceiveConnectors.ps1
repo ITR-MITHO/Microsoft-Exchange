31dab833faba911daea5c80b5b2c7080f97b45c4
Mikkel Thodsen <123701392+ITR-MITHO@users.noreply.github.com>
2023-08-03 11:48:21 +0200

    Update Compare-ReceiveConnectors.ps1

--- Compare-ReceiveConnectors.ps1
+++ Compare-ReceiveConnectors.ps1
@@ -5,20 +5,26 @@ $Connector2 = Read-Host "Name of the difference connector"
 $Range1 = (Get-ReceiveConnector $Connector1).RemoteIPRanges | Sort-Object | Select Expression
 $Range2 = (Get-ReceiveConnector $Connector2).RemoteIPRanges | Sort-Object | Select Expression
 
-
-Write-Host "=> Means the value can only be found in the Difference object" -ForegroundColor Yellow
-Write-Host "<= Means the value can only be found in the Reference object" -ForegroundColor Yellow
 $Compare = Compare-Object -ReferenceObject ($Range1).Expression -DifferenceObject ($Range2).Expression
-
 If ($Compare.SideIndicator -EQ "<=")
-
 {
+$Difference = ($Compare | Where SideIndicator -EQ "<=").InputObject
+Echo "
 
-$Difference = $Compare | Where SideIndicator -EQ "<=" | Select InputObject
+$Connector2 is missing the following IP's from $Connector1
 
-Write-Host "
-
-$Connector2 is missing the following IP's from $Connector1" -ForegroundColor Red
+"
 $Difference
+}
 
+$Compare2 = Compare-Object -ReferenceObject ($Range1).Expression -DifferenceObject ($Range2).Expression
+If ($Compare2.SideIndicator -EQ "=>")
+{
+$Reference = ($Compare2 | Where SideIndicator -EQ "=>").InputObject
+Echo "
+
+$Connector1 is missing the following IP's from $Connector2
+
+"
+$Reference
 }

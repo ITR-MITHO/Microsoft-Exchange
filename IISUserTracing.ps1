de9ebe622268f73edc673b380120bd1a64f565a3
Mikkel Thodsen <123701392+ITR-MITHO@users.noreply.github.com>
2023-12-07 11:50:13 +0100

    Update IISUserTracing.ps1

--- IISUserTracing.ps1
+++ IISUserTracing.ps1
@@ -1,15 +1,17 @@
-   <# 
+<# 
    
-   The script will collect the 500 newest entries from a specific user in the IIS logs the past X days on the following services: 
+   The script will collect the newest entries from a specific user in the IIS logs the past X days on the following services: 
    Autodiscover, Exchange Web Services and MAPI
-   Change the variable $Days to something else if you want it to be higher than 10 days.
+
+   Change the variable $Days to something else if you want it to be higher than 1 day.
    The only thing you need to do, is to run this script as elevated, and enter a username or IP-address to search for.
   
    #>
 
 Import-Module WebAdministration
 $Folder = Get-ItemProperty "IIS:\Sites\Default Web Site" -name logFile.directory | Select Value
-$Days = 10
+$Days = 1
+$Date = (Get-Date).AddDays(-$Days)
 $PMError = Test-Path $Home\desktop\ExchangeLogs
 if ($PMError)
 {
@@ -30,12 +32,11 @@ mkdir $home\desktop\ExchangeLogs | Out-null
     CD $Folder.Value
 }
 CLS
-Write-Host "INFORMATION: Searching for $User - This can take 2-5 minutes." -foregroundcolor Yellow
-$Data = Get-ChildItem -Recurse | Sort-Object -Descending | Select -First $Days
+Write-Host "INFORMATION: Searching for $User in IIS logs for the past $Days days." -foregroundcolor Yellow
+$Data = Get-ChildItem -Recurse | Where {$_.LastWriteTime -GT $Date} | Sort-Object -Descending
 
-$Data | Select-String -Pattern "$User" | Where {$_.Line -like "*/MAPI/*"} | Sort-Object -Descending | Select -first 500 > $home\Desktop\ExchangeLogs\MAPI.log
-$Data | Select-String -Pattern "$User" | Where {$_.Line -like "*/Autodiscover/*"} | Sort-Object -Descending | Select -first 500 > $home\Desktop\ExchangeLogs\Autodiscover.log
-$Data | Select-String -Pattern "$User" | Where {$_.Line -like "*/EWS/*"} | Sort-Object -Descending | Select -first 500 > $home\Desktop\ExchangeLogs\EWS.log
+$Data | Select-String -Pattern "$User" | Where {$_.Line -like "*/MAPI/*"} | Sort-Object -Descending > $home\Desktop\ExchangeLogs\MAPI.log
+$Data | Select-String -Pattern "$User" | Where {$_.Line -like "*/Autodiscover/*"} | Sort-Object -Descending > $home\Desktop\ExchangeLogs\Autodiscover.log
+$Data | Select-String -Pattern "$User" | Where {$_.Line -like "*/EWS/*"} | Sort-Object -Descending > $home\Desktop\ExchangeLogs\EWS.log
 
-CLS
-Write-Host "INFORMATION: Completed! Find your files $Home\Desktop\Exchangelogs" -ForegroundColor Green
+Write-Host "INFORMATION: Find your log files here: $Home\Desktop\Exchangelogs" -ForegroundColor Green

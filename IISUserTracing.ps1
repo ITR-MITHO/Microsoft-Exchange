3e152def152a1153f4c1fc7165450a55e4bd6dee
Mikkel Thodsen <123701392+ITR-MITHO@users.noreply.github.com>
2023-12-08 08:42:44 +0000

    Update IISUserTracing.ps1

--- IISUserTracing.ps1
+++ IISUserTracing.ps1
@@ -1,7 +1,7 @@
 <# 
    
    The script will collect the newest entries from a specific user in the IIS logs the past X days on the following services: 
-   Autodiscover, Exchange Web Services and MAPI
+   Autodiscover, Exchange Web Services, MAPI and ActiveSync
 
    Change the variable $Days to something else if you want it to be higher than 1 day.
    The only thing you need to do, is to run this script as elevated, and enter a username or IP-address to search for.
@@ -12,17 +12,13 @@ Import-Module WebAdministration
 $Folder = Get-ItemProperty "IIS:\Sites\Default Web Site" -name logFile.directory | Select Value
 $Days = 1
 $Date = (Get-Date).AddDays(-$Days)
-$PMError = Test-Path $Home\desktop\ExchangeLogs
-if ($PMError)
-{
-
-}
-Else
+$DesktopFolder = Test-Path $Home\desktop\ExchangeLogs
+if (-not $Desktopfolder)
 {
 mkdir $home\desktop\ExchangeLogs | Out-null
 }
    $User = Read-Host "Enter the username of the account you'd like to search for or the IP-address you are interrested in"
-
+   
    If ($Folder.Value -like "%Systemdrive%*")
 {
     CD "C:\Inetpub\Logs\LogFiles\W3SVC1"
@@ -38,5 +34,7 @@ $Data = Get-ChildItem -Recurse | Where {$_.LastWriteTime -GT $Date} | Sort-Objec
 $Data | Select-String -Pattern "$User" | Where {$_.Line -like "*/Autodiscover/*"} | Sort-Object -Descending > $home\Desktop\ExchangeLogs\Autodiscover.log
 $Data | Select-String -Pattern "$User" | Where {$_.Line -like "*/EWS/*"} | Sort-Object -Descending > $home\Desktop\ExchangeLogs\EWS.log
 $Data | Select-String -Pattern "$User" | Where {$_.Line -like "*/MAPI/*"} | Sort-Object -Descending > $home\Desktop\ExchangeLogs\MAPI.log
+$Data | Select-String -Pattern "$User" | Where {$_.Line -like "*/Microsoft-Server-ActiveSync/*"} | Sort-Object -Descending > $home\Desktop\ExchangeLogs\ActiveSync.log
+
 
 Write-Host "INFORMATION: Find your log files here: $Home\Desktop\Exchangelogs" -ForegroundColor Green

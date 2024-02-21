0363146820c4c134bc950a7c0bb24065d9b4ff81
Mikkel Thodsen <123701392+ITR-MITHO@users.noreply.github.com>
2024-02-21 08:25:39 +0100

    Update EnableKerberos.ps1

--- EnableKerberos.ps1
+++ EnableKerberos.ps1
@@ -9,6 +9,9 @@ Import-Module ActiveDirectory
 # Verify that the SPN isn't already taken by another service.
 Setspn -F -Q http/mail.domain.com
 
+# Verify that ASA isn't already created
+Get-ClientAccessServer $env:computername -IncludeAlternateServiceAccountCredentialStatus | Format-List Name, AlternateServiceAccountConfiguration
+
 # Create a new ASA-credential named "EXCH-KERB" - Can be found in AD by searching for 'computers' when created
 New-ADComputer -Name EXCH-KERB -AccountPassword (Read-Host 'Enter password' -AsSecureString) -Description 'Alternate Service Account credentials for Exchange' -Enabled:$True -SamAccountName EXCH-KERB
 Set-ADComputer EXCH-KERB -add @{"msDS-SupportedEncryptionTypes"="28"}
@@ -18,7 +21,7 @@ cd $Exscripts
 .\RollAlternateServiceAccountPassword.ps1 -ToSpecificServer localhost -GenerateNewPasswordFor domain.com\EXCH-KERB$
 
 # Verify that the ASA-credential is created
-Get-ClientAccessServer localhost -IncludeAlternateServiceAccountCredentialStatus | Format-List Name, AlternateServiceAccountConfiguration
+Get-ClientAccessServer $env:computername -IncludeAlternateServiceAccountCredentialStatus | Format-List Name, AlternateServiceAccountConfiguration
 
 # Create a SPN that matches your autodiscover URL
 setspn -S http/mail.domain.com domain.com\EXCH-KERB$
@@ -27,8 +30,8 @@ setspn -S http/mail.domain.com domain.com\EXCH-KERB$
  setspn -L domain.com\EXCH-KERB$
 
 # Enable kerberos authentication for Outlook clients:
-Get-OutlookAnywhere -Server $env:localhost | Set-OutlookAnywhere -InternalClientAuthenticationMethod  Negotiate
-Get-MapiVirtualDirectory -Server $env:localhost | Set-MapiVirtualDirectory -IISAuthenticationMethods OAuth,Kerberos
+Get-OutlookAnywhere -Server $env:computername | Set-OutlookAnywhere -InternalClientAuthenticationMethod  Negotiate
+Get-MapiVirtualDirectory -Server $env:computername | Set-MapiVirtualDirectory -IISAuthenticationMethods OAuth,Kerberos
  
 <#
 Verify traffic is using Kerberos:

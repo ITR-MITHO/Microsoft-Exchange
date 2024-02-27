6015752fcdc36000d88497489a086c968953c365
Mikkel Thodsen <123701392+ITR-MITHO@users.noreply.github.com>
2024-02-27 12:53:24 +0100

    Update CertExpireAlert.ps1

--- CertExpireAlert.ps1
+++ CertExpireAlert.ps1
@@ -1,18 +1,11 @@
 $Date = (Get-Date).AddDays(-60)
 $Certificate = Get-ExchangeCertificate | Where {$_.NotAfter -lt "$Date"} | Select Thumbprint, FriendlyName, Subject, Notafter
 
+CLS
+Write-Host "Certificates that expires in less than 60 days.." -ForeGroundColor Yellow
 Foreach ($Cert in $Certificate) {
 if ($Certificate) {
 $CertName = $Cert.Thumbprint
 $CertFriendly = $Cert.friendlyname
 $CertSubject = $Cert.Subject
 $CertExpire = $Cert.NotAfter
-
-# Change TO and FROM
-Send-MailMessage -To MyEmail@Domain.com -From Exchange@domain1.com -Subject "Certificate expires" -Body "
- Certificate Name: $Certname
- Friendly Name: $CertFriendly
- Subject: $CertSubject
- Expiration date: $CertExpire" -SmtpServer localhost
-}
-}

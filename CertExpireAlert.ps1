558d4a65d1fc5f9ab99d476b86abc678afa632f3
Mikkel Thodsen <123701392+ITR-MITHO@users.noreply.github.com>
2024-02-27 15:27:29 +0100

    Update CertExpireAlert.ps1

--- CertExpireAlert.ps1
+++ CertExpireAlert.ps1
@@ -1,10 +1,23 @@
 $Date = (Get-Date).AddDays(-60)
-$Certificate = Get-ExchangeCertificate | Where {$_.NotAfter -lt "$Date"} | Select Thumbprint, FriendlyName, Subject, Notafter
+$Certificate = Get-ExchangeCertificate | Where {$_.NotAfter -lt "$Date"} | Select Thumbprint, FriendlyName, Subject, Notafter, services
 
 CLS
 Write-Host "Certificates that expires in less than 60 days.." -ForeGroundColor Yellow
 Foreach ($Cert in $Certificate) {
 if ($Certificate) {
-$Cert.Thumbprint, $Cert.friendlyname, $Cert.Subject, $Cert.NotAfter
+$Thumb = $Cert.Thumbprint
+$Friendly = $Cert.friendlyname
+$Subject = $Cert.Subject
+$NotAfter = $Cert.NotAfter
+$Services = $Cert.Services
+
+Write-Host "
+$Friendly
+$Subject
+$Thumb
+$NotAfter
+$Services
+
+"
 }
   }

da438d32d51b43430ad08138750eb5353fe6a762
Mikkel Thodsen <123701392+ITR-MITHO@users.noreply.github.com>
2024-02-27 12:31:24 +0100

    Create GPOThatAffectOutlook.ps1

---
+++ GPOThatAffectOutlook.ps1
@@ -0,0 +1,14 @@
+$DC = (Get-ADDomainController | Select Name -First 1).Name
+Invoke-Command -ComputerName $DC {
+
+$AllGPO = Get-GPO -All -Domain $env:SERDNSDOMAIN
+[string[]] $MatchedGPOList = @()
+
+ForEach ($GPO in $AllGPO) { 
+    $Report = Get-GPOReport -Guid $GPO.Id -ReportType XML 
+    if ($Report -match 'Outlook') { 
+        Write-Host "$($GPO.DisplayName)" -ForeGroundColor "Green"
+        $MatchedGPOList += "$($GPO.DisplayName)";
+} 
+  }
+    }

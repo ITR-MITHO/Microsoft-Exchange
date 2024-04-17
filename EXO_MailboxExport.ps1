7d385df3d86f3234da7ea616e78c764e05d8a199
Mikkel Thodsen <123701392+ITR-MITHO@users.noreply.github.com>
2024-04-17 13:53:17 +0200

    Update EXO_MailboxExport.ps1

--- EXO_MailboxExport.ps1
+++ EXO_MailboxExport.ps1
@@ -16,7 +16,24 @@ If you're having any issues with the script, please reach out to me.
 https://github.com/ITR-MITHO
 
 #>
-$Mailboxes = Get-Mailbox -ResultSize Unlimited -RecipientTypeDetails UserMailbox
+<#
+The script will export the following information from all mailboxes:
+
+                            Alias
+                            DisplayName
+                            PrimarySmtpAddress
+                            RecipientTypeDetails
+                            TotalItemSize
+                            ArchiveSize
+
+
+.NOTE
+
+If you're having any issues with the script, please reach out to me.
+https://github.com/ITR-MITHO
+
+#>
+$Mailboxes = Get-Mailbox -ResultSize Unlimited
 $Results = @()
 Write-Host "It is estimated to take 10-15 minutes for large organisations. Grab a nice cup of coffee :-)" -ForegroundColor Yellow
 Foreach ($Mailbox in $Mailboxes)
@@ -31,30 +48,29 @@ else
     $ArchiveInMB = "0"
 }
 
-$Statistics = Get-MailboxStatistics -Identity $Mailbox.SamAccountName | Select TotalItemSize, TotalDeletedItemSize
+$Statistics = Get-MailboxStatistics -Identity $Mailbox.SamAccountName | Select TotalItemSize
 if ($Statistics) 
 {
-    $Size =  [math]::Round(([long]((($Statistics.TotalItemSize.Value -split "\(")[1] -split " ")[0] -split "," -join ""))/[math]::Pow(1024,3),3)
-    $Deleted =  [math]::Round(([long]((($Statistics.TotalDeletedItemSize.Value -split "\(")[1] -split " ")[0] -split "," -join ""))/[math]::Pow(1024,3),3)
+    $SizeInMB =  [math]::Round(([long]((($Statistics.TotalItemSize.Value -split "\(")[1] -split " ")[0] -split "," -join ""))/[math]::Pow(1024,3),3)
+
 } 
 else 
 {
-    $Size = "0"
-    $Deleted = "0"
+    $SizeInMB = "0"
+
 }
 $Data = @{
-    Username = $Mailbox.SamAccountName
+    Username = $Mailbox.Alias
     Name = $Mailbox.DisplayName
     Email = $Mailbox.PrimarySmtpAddress
     Type = $Mailbox.RecipientTypeDetails
-    MailboxSize = $Size
+    MailboxSize = $SizeInMB
     ArchiveSize = $ArchiveInMB
-    Deleted = $Deleted
 }   
 $Results += New-Object PSObject -Property $Data
 }
 # Selecting the fields in a specific order instead of random.
-$Results | Select Username, Name, Email, Type, MailboxSize, ArchiveSize, Deleted | 
+$Results | Select-Object Username, Name, Email, Type, MailboxSize, ArchiveSize | 
 Export-csv "$Home\Desktop\MailboxExport.csv" -NoTypeInformation -Encoding Unicode
 
 Write-Host "

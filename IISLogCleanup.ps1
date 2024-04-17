3951275f62881d4a2af21b7a1fb859908e261f1a
Mikkel Thodsen <123701392+ITR-MITHO@users.noreply.github.com>
2024-04-17 15:49:51 +0200

    Update IISLogCleanup.ps1

--- IISLogCleanup.ps1
+++ IISLogCleanup.ps1
@@ -8,7 +8,6 @@ Import-Module WebAdministration
 $Date = (Get-Date).AddDays(-14)
 $Folder = Get-ItemProperty "IIS:\Sites\Default Web Site" -name logFile.directory | Select Value
 If ($Folder.Value -like "%Systemdrive%*")
-
 {
     Get-ChildItem C:\Inetpub\Logs\LogFiles -Recurse | Where-Object {$_.LastWriteTime -LT $Date} | Remove-Item -Force
 }
@@ -16,7 +15,6 @@ If ($Folder.Value -like "%Systemdrive%*")
 {   
     Get-ChildItem $Folder.Value -Recurse | Where-Object {$_.LastWriteTime -LT $Date} | Remove-Item -Force
 }
-
 # Clears HTTPProxy Logs that is older than $Date
 $HTTPProxy = "C:\Program Files\Microsoft\Exchange Server\V15\Logging\HttpProxy"
 Get-ChildItem $HTTPProxy -Recurse | Where-Object {$_.LastWriteTime -LT $Date} | Remove-Item -Force

0cb292e269fd2012754d0a7bac58da5e85683666
Mikkel Thodsen <123701392+ITR-MITHO@users.noreply.github.com>
2024-04-17 15:45:41 +0200

    Update IISLogCleanup.ps1

--- IISLogCleanup.ps1
+++ IISLogCleanup.ps1
@@ -1,6 +1,6 @@
 <#
 IMPORTANT: Must be run in elevated Powershell
-Will delete Exchange IIS-logs older then 14-days
+Will delete Exchange IIS and HTTPProxy logs that is older then 14-days
 
 #>
 
@@ -10,16 +10,13 @@ $Folder = Get-ItemProperty "IIS:\Sites\Default Web Site" -name logFile.directory
 If ($Folder.Value -like "%Systemdrive%*")
 
 {
-
     Get-ChildItem C:\Inetpub\Logs\LogFiles -Recurse | Where-Object {$_.LastWriteTime -lt $Date} | Remove-Item -Force
-    
-
 }
-
  Else
-
-{
-    
+{   
     Get-ChildItem $Folder.Value -Recurse | Where-Object {$_.LastWriteTime -lt $Date} | Remove-Item -Force
-
 }
+
+# Clears HTTPProxy Logs that is older than $Date
+$HTTPProxy = "C:\Program Files\Microsoft\Exchange Server\V15\Logging\HttpProxy"
+Get-ChildItem $HTTPProxy -Recurse | Where-Object {$_.LastWriteTime -LT $Date} | Remove-Item -Force

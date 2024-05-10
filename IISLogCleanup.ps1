005e4175b2bf7f67ca345b36211afcd107e0319e
Mikkel Thodsen <123701392+ITR-MITHO@users.noreply.github.com>
2024-05-10 11:54:55 +0200

    Update IISLogCleanup.ps1

--- IISLogCleanup.ps1
+++ IISLogCleanup.ps1
@@ -20,20 +20,20 @@ $Date = (Get-Date).AddDays(-14)
 $Folder = Get-ItemProperty "IIS:\Sites\Default Web Site" -name logFile.directory | Select Value
 If ($Folder.Value -like "%Systemdrive%*")
 {
-    Get-ChildItem C:\Inetpub\Logs\LogFiles -Recurse | Where-Object {$_.LastWriteTime -LT $Date -and $_.Name -like "*.log"} | Remove-Item -Force
+    Get-ChildItem C:\Inetpub\Logs\LogFiles -Recurse | Where-Object {$_.LastWriteTime -LT $Date -and $_.Name -like "*.LOG"} | Remove-Item -Force
 }
  Else
 {   
-    Get-ChildItem $Folder.Value -Recurse | Where-Object {$_.LastWriteTime -LT $Date} | Remove-Item -Force
+    Get-ChildItem $Folder.Value -Recurse | Where-Object {$_.LastWriteTime -LT $Date -and $_.Name -like "*.LOG"} | Remove-Item -Force
 }
 
 # Deletes HTTPProxy logs that is older than 5 days
 $ProxyDate = (Get-Date).AddDays(-5)
-Get-ChildItem "$($env:ExchangeInstallPath)Logging\HttpProxy" -Recurse | Where-Object {$_.LastWriteTime -LT $ProxyDate -and $_.Name -like "*.log"} | Remove-Item -Force
+Get-ChildItem "$($env:ExchangeInstallPath)Logging\HttpProxy" -Recurse | Where-Object {$_.LastWriteTime -LT $ProxyDate -and $_.Name -like "*.LOG"} | Remove-Item -Force
 
 # Deletes Exchange service logs that is older than 10 days
 $ExchangeDate = (Get-Date).AddDays(-10)
-Get-ChildItem "$($env:ExchangeInstallPath)Logging\Ews" -Recurse | Where-Object {$_.LastWriteTime -LT $ExchangeDate -and $_.Name -like "*.log"} | Remove-Item -Force
+Get-ChildItem "$($env:ExchangeInstallPath)Logging\Ews" -Recurse | Where-Object {$_.LastWriteTime -LT $ExchangeDate -and $_.Name -like "*.LOG"} | Remove-Item -Force
 Get-ChildItem "$($env:ExchangeInstallPath)Logging\Autodiscover" -Recurse | Where-Object {$_.LastWriteTime -LT $ExchangeDate -and $_.Name -like "*.LOG"} | Remove-Item -Force
 Get-ChildItem "$($env:ExchangeInstallPath)Logging\MapiHttp" -Recurse | Where-Object {$_.LastWriteTime -LT $ExchangeDate -and $_.Name -like "*.LOG"} | Remove-Item -Force
 If (Test-Path "$($env:ExchangeInstallPath)Logging\Imap4")

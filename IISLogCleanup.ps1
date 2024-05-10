96959844ffcc976ddfc89c54e969a93fe3cfa887
Mikkel Thodsen <123701392+ITR-MITHO@users.noreply.github.com>
2024-05-10 11:51:10 +0200

    Update IISLogCleanup.ps1

--- IISLogCleanup.ps1
+++ IISLogCleanup.ps1
@@ -1,4 +1,20 @@
-# Deletes IIS logs that is older then 14-days
+<#
+Deletes logs in the following directories: 
+
+LOGS OLDER THAN 14 DAYS
+inetpub\logs\LogFiles
+
+LOGS OLDER THAN 5 DAYS
+$($env:ExchangeInstallPath)Logging\HttpProxy
+
+LOGS OLDER THAN 10 DAYS
+$($env:ExchangeInstallPath)Logging\Autodiscover
+$($env:ExchangeInstallPath)Logging\EWS
+$($env:ExchangeInstallPath)Logging\IMAP
+$($env:ExchangeInstallPath)Logging\MAPI
+$($env:ExchangeInstallPath)Logging\POP3
+
+#>
 Import-Module WebAdministration
 $Date = (Get-Date).AddDays(-14)
 $Folder = Get-ItemProperty "IIS:\Sites\Default Web Site" -name logFile.directory | Select Value
@@ -13,12 +29,18 @@ If ($Folder.Value -like "%Systemdrive%*")
 
 # Deletes HTTPProxy logs that is older than 5 days
 $ProxyDate = (Get-Date).AddDays(-5)
-$HTTPProxy = "C:\Program Files\Microsoft\Exchange Server\V15\Logging\HttpProxy"
-Get-ChildItem $HTTPProxy -Recurse | Where-Object {$_.LastWriteTime -LT $ProxyDate -and $_.Name -like "*.log"} | Remove-Item -Force
+Get-ChildItem "$($env:ExchangeInstallPath)Logging\HttpProxy" -Recurse | Where-Object {$_.LastWriteTime -LT $ProxyDate -and $_.Name -like "*.log"} | Remove-Item -Force
 
 # Deletes Exchange service logs that is older than 10 days
 $ExchangeDate = (Get-Date).AddDays(-10)
-Get-ChildItem "C:\Program Files\Microsoft\Exchange Server\V15\Logging\Ews" -Recurse | Where-Object {$_.LastWriteTime -LT $ExchangeDate -and $_.Name -like "*.log"} | Remove-Item -Force
-Get-ChildItem "C:\Program Files\Microsoft\Exchange Server\V15\Logging\Autodiscover" -Recurse | Where-Object {$_.LastWriteTime -LT $ExchangeDate -and $_.Name -like "*.LOG"} | Remove-Item -Force
-Get-ChildItem "C:\Program Files\Microsoft\Exchange Server\V15\Logging\MapiHttp" -Recurse | Where-Object {$_.LastWriteTime -LT $ExchangeDate -and $_.Name -like "*.LOG"} | Remove-Item -Force
-Get-ChildItem "C:\Program Files\Microsoft\Exchange Server\V15\Logging\Imap4" -Recurse | Where-Object {$_.LastWriteTime -LT $ExchangeDate -and $_.Name -like "*.LOG"} | Remove-Item -Force
+Get-ChildItem "$($env:ExchangeInstallPath)Logging\Ews" -Recurse | Where-Object {$_.LastWriteTime -LT $ExchangeDate -and $_.Name -like "*.log"} | Remove-Item -Force
+Get-ChildItem "$($env:ExchangeInstallPath)Logging\Autodiscover" -Recurse | Where-Object {$_.LastWriteTime -LT $ExchangeDate -and $_.Name -like "*.LOG"} | Remove-Item -Force
+Get-ChildItem "$($env:ExchangeInstallPath)Logging\MapiHttp" -Recurse | Where-Object {$_.LastWriteTime -LT $ExchangeDate -and $_.Name -like "*.LOG"} | Remove-Item -Force
+If (Test-Path "$($env:ExchangeInstallPath)Logging\Imap4")
+{
+Get-ChildItem "$($env:ExchangeInstallPath)Logging\Imap4" -Recurse | Where-Object {$_.LastWriteTime -LT $ExchangeDate -and $_.Name -like "*.LOG"} | Remove-Item -Force
+}
+If (Test-Path "$($env:ExchangeInstallPath)Logging\POP3")
+{
+Get-ChildItem "$($env:ExchangeInstallPath)Logging\POP3" -Recurse | Where-Object {$_.LastWriteTime -LT $ExchangeDate -and $_.Name -like "*.LOG"} | Remove-Item -Force
+}

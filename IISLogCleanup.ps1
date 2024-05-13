7392f1f927e0f5574be0b4ed016b4ba7417a5953
Mikkel Thodsen <123701392+ITR-MITHO@users.noreply.github.com>
2024-05-13 08:51:57 +0200

    Update IISLogCleanup.ps1

--- IISLogCleanup.ps1
+++ IISLogCleanup.ps1
@@ -2,7 +2,6 @@
 Deletes .LOG files older than 10 days in the following directories: 
 
 inetpub\logs\LogFiles
-$($env:ExchangeInstallPath)Logging\HttpProxy
 $($env:ExchangeInstallPath)Logging\*
 
 #>
@@ -11,12 +10,11 @@ $Days = (Get-Date).AddDays(-10)
 $Folder = Get-ItemProperty "IIS:\Sites\Default Web Site" -name logFile.directory | Select Value
 If ($Folder.Value -like "%Systemdrive%*")
 {
-    Get-ChildItem C:\Inetpub\Logs\LogFiles -Recurse | Where-Object {$_.LastWriteTime -LT $Days -and $_.Name -like "*.LOG"} | Remove-Item -Force
+    Get-ChildItem C:\Inetpub\Logs\LogFiles -Recurse | Where-Object {$_.LastWriteTime -LT $Days -and $_.Name -like "*.LOG"} | Remove-Item -Force -ErrorAction SilentlyContinue
 }
  Else
 {   
-    Get-ChildItem $Folder.Value -Recurse | Where-Object {$_.LastWriteTime -LT $Days -and $_.Name -like "*.LOG"} | Remove-Item -Force
+    Get-ChildItem $Folder.Value -Recurse | Where-Object {$_.LastWriteTime -LT $Days -and $_.Name -like "*.LOG"} | Remove-Item -Force -ErrorAction SilentlyContinue
 }
 
-Get-ChildItem "$($env:ExchangeInstallPath)Logging\HttpProxy" -Recurse | Where-Object {$_.LastWriteTime -LT $Days -and $_.Name -like "*.LOG"} | Remove-Item -Force
-Get-ChildItem "$($env:ExchangeInstallPath)Logging" -Recurse | Where-Object {$_.LastWriteTime -LT $Days -and $_.Name -like "*.LOG"} | Remove-Item -Force
+Get-ChildItem "$($env:ExchangeInstallPath)Logging" -Recurse | Where-Object {$_.LastWriteTime -LT $Days -and $_.Name -like "*.LOG"} | Remove-Item -Force -ErrorAction SilentlyContinue

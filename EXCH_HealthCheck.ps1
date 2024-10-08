b7c2b4a63d84ab4b17542eddf92167158df02b35
MITHO <123701392+ITR-MITHO@users.noreply.github.com>
2024-10-08 13:59:21 +0200

    Update EXCH_HealthCheck.ps1

--- EXCH_HealthCheck.ps1
+++ EXCH_HealthCheck.ps1
@@ -35,7 +35,7 @@ $Space = get-psdrive c | % { $_.free/($_.used + $_.free) } | % tostring p
 If ($Space -lt "20 %")
 {
 
-Write-Host "There is $space left on the C-drive. It have needs atleast 20%
+Write-Host "There is $space left on the C-drive. It needs atleast 20%
 " -ForegroundColor Red
 
 Write-host "

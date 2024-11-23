b2215ee9517d941cfe79f02c6925523a1e01fc74
MITHO <123701392+ITR-MITHO@users.noreply.github.com>
2024-11-23 01:10:28 +0100

    Update EXCH_SetupOverview.ps1

--- EXCH_SetupOverview.ps1
+++ EXCH_SetupOverview.ps1
@@ -306,22 +306,122 @@ Get-OrganizationConfig | fl OAuth2ClientProfileEnabled, MitigationsEnabled, Mapi
 
 Write-Host "
 ###########################################################################
-DMARC Records for all domains.
+Domain Checker - Looking up all DNS records
 ###########################################################################
 " -ForegroundColor Yellow
 $ErrorActionPreference = 'SilentlyContinue'
-Sleep 5
-$Domains = Get-AcceptedDomain | Where DomainName -notlike "*.local" | Select DomainName
+$Domains = Get-AcceptedDomain | Where-Object {$_.DomainName -notlike "*.local" -and $_.DomainName -notlike "*.onmicrosoft.com"}  | Select DomainName
 $Result = foreach ($Domain in $Domains) {
-$ActualDomain = $Domain.DomainName
-    Echo "---------------------- $ActualDomain ----------------------"
-    Echo ""
-    Echo "DMARC TXT Record:"
-    (nslookup -q=txt _dmarc.$ActualDomain | Select-String "DMARC1") -replace "`t", ""
-    Echo ""
+$DomainName = ($Domain.DomainName).Address
 
+# MX Records
+$MX = nslookup -q=mx $DomainName 8.8.8.8 2>$null
+$MXGrabber = $MX | Select-String "mail exchanger"
+
+# SPF Records
+$SPF = nslookup -q=txt $DomainName 8.8.8.8 2>$null
+$SPFGrabber = $SPF | Select-String "spf"
+
+# DMARC Records
+$DMARC = nslookup -q=txt _dmarc.$DomainName 8.8.8.8 2>$null
+$DMARCGrabber = $DMARC | Select-String "v=DMARC1"
+
+# Random SPF Record
+$RandomSPF = nslookup -q=txt randomspfrecord.$DomainName 8.8.8.8 2>$null
+$RandomSPFGrabber = $RandomSPF | Select-String "v=spf1"
+
+# Selector1 CNAME Record
+$Selector1 = nslookup -q=cname "selector1._domainkey.$DomainName" 8.8.8.8 2>$null
+$Selector1Grabber = $Selector1 | Select-String "canonical name"
+
+# Selector2 CNAME Record
+$Selector2 = nslookup -q=cname "selector2._domainkey.$DomainName" 8.8.8.8 2>$null
+$Selector2Grabber = $Selector2 | Select-String "canonical name"
+
+# Display Results
+Echo "--- DNS Information for $DomainName ---" >> $home\Desktop\DomainChecker.txt
+
+# MX Record
+If ($MXGrabber) {
+    $MXRecord = $MXGrabber.Line -replace ".*mail exchanger = ", ""
+    Echo "MX-Record:
+$MXRecord
+    " >> $home\Desktop\DomainChecker.txt
+} Else {
+    Echo "MX-record:
+No valid MX-record found for $DomainName
+    " >> $home\Desktop\DomainChecker.txt
 }
-$Result
+
+# SPF Record
+If ($SPFGrabber) {
+    $SPFRecord = $SPFGrabber.Line -replace ".*text = ", "" -replace '"', ""  # Clean up result
+    Echo "SPF-record:
+$SPFRecord
+    " >> $home\Desktop\DomainChecker.txt
+    
+} Else {
+    Echo "SPF-record:
+No valid SPF-record found for $DomainName
+    " >> $home\Desktop\DomainChecker.txt
+}
+
+# Random SPF Record
+If ($RandomSPFGrabber) {
+    $RandomSPF = $RandomSPFGrabber.Line -replace ".*text = ", "" -replace '"', ""  # Clean up result
+    Echo "Random SPF:
+$RandomSPF
+    " >> $home\Desktop\DomainChecker.txt
+    
+} Else {
+    Echo "Random SPF:
+No Random SPF found for $DomainName
+    " >> $home\Desktop\DomainChecker.txt
+}
+
+
+# DMARC Record
+If ($DMARCGrabber) {
+    $DMARCRecord = $DMARCGrabber.Line -replace ".*text = ", "" -replace '"', ""  # Clean up result
+ Echo "DMARC-record:
+$DMARCRecord
+    " >> $home\Desktop\DomainChecker.txt
+    
+} Else {
+    Echo "DMARC-record:
+No valid DMARC-record found for $DomainName
+    " >> $home\Desktop\DomainChecker.txt
+}
+
+# Selector1 Record
+If ($Selector1Grabber) {
+$S1 = $Selector1Grabber.Line -replace ".*canonical name = ", ""
+ Echo "Selector1:
+$S1
+    " >> $home\Desktop\DomainChecker.txt
+    
+} Else {
+    Echo "Selector1:
+No CNAME found for Selector1 $DomainName
+    " >> $home\Desktop\DomainChecker.txt
+}
+
+
+# Selector2 Rcord
+If ($Selector2Grabber) {
+$S2 = $Selector2Grabber.Line -replace ".*canonical name = ", ""
+ Echo "Selector2:
+$S2
+    " >> $home\Desktop\DomainChecker.txt
+    
+} Else {
+    Echo "Selector2:
+No CNAME found for Selector2 $DomainName
+    
+    
+    " >> $home\Desktop\DomainChecker.txt
+}
+    }
 
 
 Write-Host "
@@ -359,4 +459,4 @@ Get-ExchangeServer $env:computername | Get-OutlookAnywhere | fl InternalClientAu
 Stop-Transcript | out-null
 
 Write-host "Script completed. 
-Find your output file on your desktop here: $home\Desktop\ExchangeReport.txt" -ForegroundColor Green
+Find your output files on your desktop here: $home\Desktop\ExchangeReport.txt and $home\Desktop\DomainChecker.txt" -ForegroundColor Green

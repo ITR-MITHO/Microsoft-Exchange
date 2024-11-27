d080b4c1d1b5523f8f770d995704fcb1e609bdc8
MITHO <123701392+ITR-MITHO@users.noreply.github.com>
2024-11-27 15:19:01 +0100

    Update BOTH_UserLicenses.ps1

--- BOTH_UserLicenses.ps1
+++ BOTH_UserLicenses.ps1
@@ -35,6 +35,7 @@ Foreach ($Mailbox in $Mailboxes) {
     $MailboxType = $Mailbox.RecipientTypeDetails
     $MailboxUsername = $Mailbox.SamAccountName
     $MailboxDisplayName = $Mailbox.DisplayName
+    $ADAtt = Get-ADUser -Identity $MailboxUserName -Properties Enabled, LastLogonDate
 
 
     $License = (Get-MsolUser -UserPrincipalName $MailboxUPN -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Licenses -ErrorAction SilentlyContinue)
@@ -55,14 +56,28 @@ Foreach ($Mailbox in $Mailboxes) {
         $License = "Microsoft Business Premium"
     }
 
+
+    IF ($ADAtt.LastLogonDate)
+{
+$LastLogonDate = $ADAtt.LastlogonDate.ToString("dd-MM-yyyy")
+}
+Else
+{
+$LastLogonDate = ""
+}
+
+
     $Results += [PSCustomObject]@{
         DisplayName = $MailboxDisplayName
         Username = $MailboxUsername
         Email    = $MailboxMail
         Licens   = $License
         Type     = $MailboxType
+        Enabled  = $ADAtt.Enabled
+        LastLogon = $LastLogonDate
+
     }
 }
 
-$Results | Select-Object DisplayName, Username, Email, Licens, Type | Export-csv $home\desktop\Licenses.csv -NotypeInformation -Encoding Unicode
+$Results | Select-Object DisplayName, Username, Email, Licens, Type, Enabled, LastLogon | Export-csv $home\desktop\Licenses.csv -NotypeInformation -Encoding Unicode
 Write-Host "Export Completed, find your file here: $Home\Desktop\Licenses.csv" -ForeGroundColor Green

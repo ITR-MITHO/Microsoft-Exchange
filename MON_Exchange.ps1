6c71378be7edf95f132412856152d46c65821d15
MITHO <123701392+ITR-MITHO@users.noreply.github.com>
2025-01-29 13:21:25 +0100

    Update MON_Exchange.ps1

--- MON_Exchange.ps1
+++ MON_Exchange.ps1
@@ -1,15 +1,8 @@
+# Parameters
+
 $Domain = (Get-Accepteddomain | Where {$_.Default -EQ "True"}).Name
 $Sender = "ITM8_Exchange@$domain"
 $Subject = "Customer: $Domain - Server: $Env:computername"
-
-$ExchangeDisplay = Get-ChildItem HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall | ForEach-Object {
-    $DisplayName = $_.GetValue("DisplayName")
-    if ($DisplayName -match "Microsoft Exchange Server 20*") {
-        [PSCustomObject]@{
-            DisplayName = $DisplayName}
-                                        }
-                                            }
-
 $Mailbox = (Get-Mailbox).Count
 $RemoteMailbox = (Get-RemoteMailbox).count
 $ExchVer = (Get-Command Exsetup.exe).Version
@@ -26,10 +19,9 @@ Else
 $Hybrid = "False"
 }
 
-
+# Output
 $Body = "
 Servername: $env:computername
-Exchange Display: $ExchangeDisplay
 Exchange Version: $ExchVer
 Forest Level: $ForestLevel
 Domain Level: $DomainLevel
@@ -39,8 +31,6 @@ Hybrid Enabled: $Hybrid
 
 
 "
+
+# Send mail
 Send-MailMessage -From $Sender -to exchangeteam@itm8.com -Subject $Subject -SmtpServer localhost -Body $Body
-
-
-
-

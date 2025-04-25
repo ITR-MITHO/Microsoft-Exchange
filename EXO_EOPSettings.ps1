7900b42365d9d1213283a6da7574abb3a4b874e3
MITHO <123701392+ITR-MITHO@users.noreply.github.com>
2025-04-25 17:19:22 +0200

    Update EXO_EOPSettings.ps1

--- EXO_EOPSettings.ps1
+++ EXO_EOPSettings.ps1
@@ -15,7 +15,7 @@
     - Anti-Phishing Policy
     - Inbound Anti-Spam Policy
     - Outbound Anti-Spam Policy
-    - Perhaps... Custom Quarantine Policies?
+    - Perhaps... Custom Quarantine Policies, that enables notifications on all 3 policies?
 
 #>
 # Enable MailTips, Audit Log and Notify users about External Senders

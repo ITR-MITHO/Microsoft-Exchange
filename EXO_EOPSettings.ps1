669475c5dee26e782eea6e2794cc978f4c4b3934
MITHO <123701392+ITR-MITHO@users.noreply.github.com>
2025-04-25 14:40:31 +0200

    Update EXO_EOPSettings.ps1

--- EXO_EOPSettings.ps1
+++ EXO_EOPSettings.ps1
@@ -40,12 +40,11 @@ $SafeLinks = @{
 	DisableUrlRewrite		= $false
 }
 New-SafeLinksPolicy @Safelinks
-# Create the rule for all users in all valid domains and associate with policy.
 New-SafeLinksRule -Name "ITM8 - Safe Links Policy" -SafeLinksPolicy "ITM8 - Safe Links Policy" -RecipientDomainIs (Get-AcceptedDomain).Name -Priority 0 -Enabled $true
 
 # New Anti-Phishing Policy
 $AntiPhish = @{
-	Name = "ITM8 - Anti-Phishing policy"
+	Name 					= "ITM8 - Anti-Phishing policy"
     	AdminDisplayName 			= "ITM8 - Anti-Phishing policy"
     	PhishThresholdLevel             	= 3
 	EnableTargetedUserProtection		= $true
@@ -53,11 +52,6 @@ $AntiPhish = @{
 	EnableMailboxIntelligence		= $true
 	EnableMailboxIntelligenceProtection	= $true
 	EnableSpoofIntelligence			= $true
-}
-New-AntiPhishPolicy @AntiPhish
-
-# Anti-Phish Policy Settings
-$Actions = @{
 	TargetedUserProtectionAction		= "Quarantine"
 	TargetedDomainProtectionAction		= "Quarantine"
 	MailboxIntelligenceProtectionAction	= "MoveToJmf"
@@ -70,15 +64,13 @@ $Actions = @{
 	EnableUnusualCharactersSafetyTips 	= $true
 	EnableUnauthenticatedSender 		= $true
 	EnableViaTag 				= $true
-    	EnableSpoofIntelligence             	= $true
-    	HonorDmarcPolicy                    	= $true
-    	SpoofQuarantineTag                  	= "DefaultFullAccessPolicy"
+    	HonorDmarcPolicy                   	= $true
+    	SpoofQuarantineTag                 	= "DefaultFullAccessPolicy"
 	TargetedDomainQuarantineTag		= "DefaultFullAccessWithNotificationPolicy"
- 	MailboxIntelligenceQuarantineTag	= "DefaultFullAccessPolicy"
-  	TargetedUserQuarantineTag		= "DefaultFullAccessWithNotificationPolicy"
-  	
-}
-Set-AntiPhishPolicy -Identity "ITM8 - Anti-Phishing policy" @Actions
+	MailboxIntelligenceQuarantineTag	= "DefaultFullAccessPolicy"
+	TargetedUserQuarantineTag		= "DefaultFullAccessWithNotificationPolicy"
+}	
+New-AntiPhishPolicy @AntiPhish
 New-AntiPhishRule -Name "ITM8 - Anti-Phishing policy" -AntiPhishPolicy "ITM8 - Anti-Phishing policy" -RecipientDomainIs (Get-AcceptedDomain).Name -Enabled $true -Priority 0
 
 # New Inbound Anti-Spam Policy
@@ -88,38 +80,30 @@ $AntiSpam = @{
 	HighConfidenceSpamAction		 = "Quarantine"
 	BulkSpamAction	                    	 = "MoveToJmf"
  	PhishSpamAction				 = "Quarantine"
+	IncreaseScoreWithImageLinks		 = "Off"
+	IncreaseScoreWithNumericIps		 = "Off"
+	IncreaseScoreWithRedirectToOtherPort	 = "Off"
+	IncreaseScoreWithBizOrInfoUrls		 = "Off"
+	MarkAsSpamBulkMail			 = "On"
+	MarkAsSpamEmptyMessages			 = "Off"
+	MarkAsSpamEmbedTagsInHtml 		 = "Off"
+	MarkAsSpamFormTags 			 = "On"
+	MarkAsSpamFrames 			 = "On"
+	MarkAsSpamJavaScript 			 = "Off"
+	MarkAsSpamWebBugsInHtml 		 = "Off"
+	MarkAsSpamObjectTags 			 = "On"
+	MarkAsSpamSensitiveWordList 		 = "Off"
+	MarkAsSpamSpfRecordHardFail 		 = "Off"
+	MarkAsSpamFromAddressAuthFail 		 = "Off"
+	MarkAsSpamNdrBackscatter 		 = "Off"
+	BulkThreshold 				 = "6"
+	SpamQuarantineTag 			 = "DefaultFullAccessPolicy"
+	HighConfidenceSpamQuarantineTag 	 = "DefaultFullAccessWithNotificationPolicy"
+	BulkQuarantineTag 			 = "DefaultFullAccessPolicy"
+	PhishQuarantineTag 			 = "DefaultFullAccessWithNotificationPolicy"
+	HighConfidencePhishQuarantineTag 	 = "AdminOnlyAccessPolicy"
+	QuarantineRetentionPeriod 		 = "30"
 }
 New-HostedContentFilterPolicy @AntiSpam
-
-# Anti-Spam Policy Settings
-$AntiSpamSettings = @{
-Identity 					  = "ITM8 - Inbound Anti-Spam policy"
-IncreaseScoreWithImageLinks			  = "Off"
-IncreaseScoreWithNumericIps			  = "Off"
-IncreaseScoreWithRedirectToOtherPort		  = "Off"
-IncreaseScoreWithBizOrInfoUrls			  = "Off"
-MarkAsSpamBulkMail				  = "On"
-MarkAsSpamEmptyMessages				  = "Off"
-MarkAsSpamEmbedTagsInHtml 			  = "Off"
-MarkAsSpamFormTags 				  = "On"
-MarkAsSpamFrames 				  = "On"
-MarkAsSpamJavaScript 				  = "Off"
-MarkAsSpamWebBugsInHtml 			  = "Off"
-MarkAsSpamObjectTags 				  = "On"
-MarkAsSpamSensitiveWordList 			  = "Off"
-MarkAsSpamSpfRecordHardFail 			  = "Off"
-MarkAsSpamFromAddressAuthFail 			  = "Off"
-MarkAsSpamNdrBackscatter 			  = "Off"
-BulkThreshold 					  = "6"
-SpamQuarantineTag 				  = "DefaultFullAccessPolicy"
-HighConfidenceSpamQuarantineTag 		  = "DefaultFullAccessWithNotificationPolicy"
-BulkQuarantineTag 				  = "DefaultFullAccessPolicy"
-PhishQuarantineTag 				  = "DefaultFullAccessWithNotificationPolicy"
-HighConfidencePhishQuarantineTag 		  = "AdminOnlyAccessPolicy"
-QuarantineRetentionPeriod 			  = "30"
-
-}
-Set-HostedContentFilterPolicy @AntiSpamSettings
-
-# Apply the policy to all Accepted Domains
 New-HostedContentFilterRule -Name "ITM8 - Inbound Anti-Spam policy" -HostedContentFilterPolicy "ITM8 - Inbound Anti-Spam policy" -RecipientDomainIs (Get-AcceptedDomain).Name
+

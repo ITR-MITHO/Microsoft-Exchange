6a06549ba0f28a32beca545c96996f7e52ac32b5
MITHO <123701392+ITR-MITHO@users.noreply.github.com>
2025-04-28 10:45:56 +0200

    Update EXCH_SendMailsInALoop.ps1

--- EXCH_SendMailsInALoop.ps1
+++ EXCH_SendMailsInALoop.ps1
@@ -5,7 +5,7 @@ $Recipient = "Mailbox1@itm8exchangetest.dk"
 1..100 | ForEach-Object {
     $Subject = "Test Email $_"
     $Body = "This is the body of test email number $_."
-    Send-MailMessage -To $PublicFolderSMTP -Subject $Subject -Body $Body -SmtpServer "localhost" -From "Mailbox2@itm8exchangetest.dk"
+    Send-MailMessage -To $Recipient -Subject $Subject -Body $Body -SmtpServer "localhost" -From "Mailbox2@itm8exchangetest.dk"
     Write-Host "Sent email $_"
 }
 

2d941bc38c9f1d4e488ba863deb39dfc801b1822
MITHO <123701392+ITR-MITHO@users.noreply.github.com>
2025-05-05 11:12:31 +0200

    Update MON_TSMBACKUP.ps1

--- MON_TSMBACKUP.ps1
+++ MON_TSMBACKUP.ps1
@@ -1,34 +1,31 @@
+$Customer = "CUSTOMER NAME" # Remember to change
 Add-PSSnapin *EXC*
 Import-Module ActiveDirectory
 $Domain = (Get-Accepteddomain | Where {$_.Default -EQ "True"}).Name
-$Sender = "ITM8-EXCH@$domain"
+$Sender = "ITM8-EXCH@$Domain"
 $Databases = Get-MailboxDatabase -Status | Select Name, LastFullBackup, LastIncrementalBackup
 
-# Export data about failed backups
-If (Test-Path C:\ITM8\IncrementalLog.txt)
-{
-Remove-Item C:\ITM8\IncrementalLog.txt -force
-Remove-Item C:\ITM8\FullLog.txt -force
-}
+$FullBackupBody = ""
+$IncrementalBackupBody = ""
+Foreach ($DB in $Databases) {
+    $Data = $DB.Name
+    $LastFullBackup = $DB.LastFullBackup
+    $LastIncrementalBackup = $DB.LastIncrementalBackup
 
-Foreach ($DB in $Databases)
-{
-$Data = $DB.Name
-$LastFullBackup = $DB.LastFullBackup
-$LastIncrementalBackup = $DB.LastIncrementalBackup
-If ($DB.LastFullBackup -LT (Get-date).AddDays(-8))
-{
-    Echo "$Data - Last Full: $LastFullBackup" >> C:\ITM8\FullLog.txt
-
-Send-MailMessage -to ExchangeTeam@itm8.com -From $Sender -Subject "CUSTOMER - FULL BACKUP" -SmtpServer Localhost -Attachments C:\ITM8\FullLog.txt -Body "
-Fullbackups that have failed for 8 days"
-}
-
-If ($DB.LastIncrementalBackup -LT (Get-date).AddDays(-3))
-{
-    Echo "$Data - Last Incremental: $LastIncrementalBackup" >> C:\ITM8\IncrementalLog.txt
-
-Send-MailMessage -to ExchangeTeam@itm8.com -From $Sender -Subject "CUSTOMER - INCREMENTAL BACKUP" -SmtpServer Localhost -Attachments C:\ITM8\incrementalLog.txt -Body "
-Incremental that have failed for 3 days"
-} 
+    If ($LastFullBackup -lt (Get-Date).AddDays(-8)) {
+        $FullBackupBody += "$Data - Last Full: $LastFullBackup`r`n"
     }
+
+    If ($LastIncrementalBackup -lt (Get-Date).AddDays(-3)) {
+        $IncrementalBackupBody += "$Data - Last Incremental: $LastIncrementalBackup`r`n"
+    }
+}
+
+# Send email if there is anything to report
+If ($FullBackupBody) {
+    Send-MailMessage -To ExchangeTeam@itm8.com -From $Sender -Subject "$Customer - FULL BACKUP" -SmtpServer Localhost -Body "Full backups that have failed for 8 days:`r`n`r`n$FullBackupBody"
+}
+
+If ($IncrementalBackupBody) {
+    Send-MailMessage -To ExchangeTeam@itm8.com -From $Sender -Subject "$Customer - INCREMENTAL BACKUP" -SmtpServer Localhost -Body "Incremental backups that have failed for 3 days:`r`n`r`n$IncrementalBackupBody"
+}

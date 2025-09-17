5eb631f49538d2e0dbdd4a5bad4e10792a7aaab5
MITHO <123701392+ITR-MITHO@users.noreply.github.com>
2025-09-17 15:13:12 +0200

    Update BOTH_CalendarPermissions.ps1

--- BOTH_CalendarPermissions.ps1
+++ BOTH_CalendarPermissions.ps1
@@ -7,7 +7,7 @@ $UserPrincipalName = $Mailbox.UserPrincipalName
 $Calendar = (Get-MailboxFolderStatistics -Identity $Mailbox.UserPrincipalName -FolderScope Calendar | Where { $_.FolderType -eq 'Calendar'}).Name
 Try
 { 
-    # Only use this if user is not DEFAULT # Add-MailboxFolderPermission -Identity ($Mailbox.UserPrincipalName+":\$Calendar") -User $User -AccessRights $AccessRight -SendNotificationToUser $false -ErrorAction Stop
+    # Remove this comment if user not 'Default'  Add-MailboxFolderPermission -Identity ($Mailbox.UserPrincipalName+":\$Calendar") -User $User -AccessRights $AccessRight -SendNotificationToUser $false -ErrorAction Stop
     Set-MailboxFolderPermission -Identity ($Mailbox.UserPrincipalName+":\$Calendar") -User $User -AccessRights $AccessRight -SendNotificationToUser $false -WarningAction Stop
     Write-Host "$User with $AccessRight added to $UserPrincipalName" -ForegroundColor Green
 }

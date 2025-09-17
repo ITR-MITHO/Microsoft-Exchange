eed9c2a5eafbe84af5ca44ee28826de649727831
MITHO <123701392+ITR-MITHO@users.noreply.github.com>
2025-09-17 15:09:55 +0200

    Update BOTH_CalendarPermissions.ps1

--- BOTH_CalendarPermissions.ps1
+++ BOTH_CalendarPermissions.ps1
@@ -3,7 +3,9 @@ $User = 'Default'
 $AccessRight = 'Reviewer'
 Foreach ($Mailbox in Get-Mailbox -ResultSize Unlimited -RecipientTypeDetails UserMailbox)
 {
-Try 
+$UserPrincipalName = $Mailbox.UserPrincipalName
+$Calendar = (Get-MailboxFolderStatistics -Identity $Mailbox.UserPrincipalName -FolderScope Calendar | Where { $_.FolderType -eq 'Calendar'}).Name
+Try
 { 
     Add-MailboxFolderPermission -Identity ($Mailbox.UserPrincipalName+":\$Calendar") -User $User -AccessRights $AccessRight -SendNotificationToUser $false -ErrorAction Stop
     Set-MailboxFolderPermission -Identity ($Mailbox.UserPrincipalName+":\$Calendar") -User $User -AccessRights $AccessRight -SendNotificationToUser $false -WarningAction Stop

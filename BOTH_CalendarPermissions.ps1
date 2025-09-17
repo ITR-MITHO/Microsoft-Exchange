51ec8a8fac01e51fc331fe5bc1745db156806d30
MITHO <123701392+ITR-MITHO@users.noreply.github.com>
2025-09-17 13:25:07 +0200

    Create BOTH_CalendarPermissions.ps1

---
+++ BOTH_CalendarPermissions.ps1
@@ -0,0 +1,17 @@
+# Default UserMailbox Calendar Permissions
+$User = 'Default'
+$AccessRight = 'Reviewer'
+Foreach ($Mailbox in Get-Mailbox -ResultSize Unlimited -RecipientTypeDetails UserMailbox)
+{
+Try 
+{ 
+    Add-MailboxFolderPermission -Identity ($Mailbox.UserPrincipalName+":\$Calendar") -User $User -AccessRights $AccessRight -SendNotificationToUser $false -ErrorAction Stop
+    Set-MailboxFolderPermission -Identity ($Mailbox.UserPrincipalName+":\$Calendar") -User $User -AccessRights $AccessRight -SendNotificationToUser $false -WarningAction Stop
+    Write-Host "$User with $AccessRight to $UserPrincipalName" -ForegroundColor Green
+}
+Catch
+{
+    Write-Warning "Permission already exist on Mailbox: $UserPrincipalName"
+    Continue
+}
+	}

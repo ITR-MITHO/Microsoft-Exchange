facef1d3378363621a178b23ab206c9bccf765cd
MITHO <123701392+ITR-MITHO@users.noreply.github.com>
2025-10-16 15:52:38 +0200

    Update EXCH_IPResolution.ps1

--- EXCH_IPResolution.ps1
+++ EXCH_IPResolution.ps1
@@ -1,6 +1,6 @@
 <#
 The script is designed to resolve IP's that uses Exchange to relay e-mails. 
-It will collect MessageTrackingLogs from the last 30 days and try to resolve the IPs. 
+It will collect MessageTrackingLogs from the last 5 days and try to resolve the IPs. 
 
 If it cannot resolve the IP, it will be listed as 'Unresolved' 
 
@@ -12,8 +12,8 @@ A .csv-file will be placed on your desktop named ClientIP_Resolution.csv
 
 $Results = @()
 
-$Data = Get-ExchangeServer | Get-MessageTrackingLog -ResultSize Unlimited -Start (Get-Date).AddDays(-30) -EventId Receive | 
-    Select-Object Sender, @{Name='Recipients';Expression={$_.Recipients}}, OriginalClientIP, MessageSubject
+$Data = Get-ExchangeServer | Get-MessageTrackingLog -ResultSize Unlimited -Start (Get-Date).AddDays(-5) -EventId Receive | 
+    Select-Object Sender, @{Name='Recipients';Expression={$_.Recipients}}, OriginalClientIP, MessageSubject, Timestamp
 
 foreach ($Entry in $Data) {
     $ResolvedName = $null
@@ -28,9 +28,9 @@ foreach ($Entry in $Data) {
         TimeStamp        = $Entry.TimeStamp
         Sender           = $Entry.Sender
         Recipients       = ($Entry.Recipients -join ", ")
-        Subject          = $Entry.MessageSubject
         OriginalClientIP = $Entry.OriginalClientIP
         Hostname         = $ResolvedName
+        Subject          = $Entry.MessageSubject
     }
 }
 
